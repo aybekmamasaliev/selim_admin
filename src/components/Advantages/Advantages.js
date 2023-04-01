d7bdb4e92fa6ee42523f8bdc42f3af539b62bc72
aybek <aybek.mamasaliev@bk.ru>
2023-04-01 17:21:25 +0600

    feat: upload advantages

--- src/components/Advantages/Advantages.js
+++ src/components/Advantages/Advantages.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   useAddAdvantagesMutation,
   useDelAdvantagesMutation,
@@ -8,18 +9,35 @@ import Button from "../Button/Button";
 import styles from "./Advantages.module.scss";
 
 const Advantages = () => {
-  const { data: advantages = [], isLoading } = useGetAdvantagetQuery();
+  const { data: advantages = [] } = useGetAdvantagetQuery();
   const [deleteAdvantages] = useDelAdvantagesMutation();
-  const [updateAdvantage]=usePutAdvantagesMutation();
-  const [addAdvantages]=useAddAdvantagesMutation();
-
+  const [updateAdvantage] = usePutAdvantagesMutation();
+  const [addAdvantages, { isLoading }] = useAddAdvantagesMutation();
+  const [newImage, setNewImage] = useState(null);
+  const [newTxt, setNewTxt] = useState("");
 
   const handleDeleteAdvantages = async (id) => {
-    await deleteAdvantages(id).unwrap();
+    await (id).unwrap();
   };
 
   const handleUpdateAdvantege = async (id) => {
     await updateAdvantage(id).unwrap();
+  };
+
+  const handleFileChange = (e) => {
+    setNewImage(e.target.files[0]);
+  };
+
+
+  const handleAddData =(e) => {
+    e.preventDefault();
+    const formdata = new FormData();
+    if(newImage){
+      formdata.append("image", newImage);
+    }
+    formdata.append("text", newTxt);
+
+      addAdvantages(formdata)
   };
 
   console.log(advantages);
@@ -30,9 +48,12 @@ const Advantages = () => {
         <h1 className={styles.title}>Преимущества</h1>
 
         {advantages.map((item) => (
-          <form className={styles.form}>
+          <form className={styles.form} key={item.id}>
             <p className={styles.form__field}>
-              <label htmlFor={`advantage_file_${item.id}`} className={styles.form__label}>
+              <label
+                htmlFor={`advantage_file_${item.id}`}
+                className={styles.form__label}
+              >
                 Файл
               </label>
               <input
@@ -55,8 +76,12 @@ const Advantages = () => {
                 defaultValue={item.text}
               />
             </p>
-            <Button onClick={() => handleUpdateAdvantege(item.id)}>Обновить</Button>
-            <Button onClick={()=>handleDeleteAdvantages(item.id)}>Удалить</Button>
+            <Button onClick={() => handleUpdateAdvantege(item.id)}>
+              Обновить
+            </Button>
+            <Button onClick={() => handleDeleteAdvantages(item.id)}>
+              Удалить
+            </Button>
             <hr />
           </form>
         ))}
@@ -65,7 +90,7 @@ const Advantages = () => {
       <section className={styles.section}>
         <h1 className={styles.title}>Добавить преимущества</h1>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={(e) => handleAddData(e)}>
           <p className={styles.form__field}>
             <label htmlFor="main_info__title" className={styles.form__label}>
               Файл
@@ -75,6 +100,8 @@ const Advantages = () => {
               type="file"
               id="main_info__title"
               name="title"
+              // value={newImage}
+              onChange={(e) => handleFileChange(e)}
             />
           </p>
           <p className={styles.form__field}>
@@ -84,9 +111,17 @@ const Advantages = () => {
               type="text"
               id="main_info__subtitle"
               name="subtitle"
+              onChange={(e) => setNewTxt(e.target.value)}
+              value={newTxt}
             />
           </p>
-          <Button>Добавить</Button>
+          {/* <Button type="submit" disabled={!newImage || isLoading}>
+            {" "}
+            {isLoading ? "Uploading..." : "Upload"}
+          </Button> */}
+          <button type="submit" disabled={!newImage|| isLoading} >
+            {isLoading ? "Uploading..." : "Upload"}
+          </button>
         </form>
       </section>
     </>
